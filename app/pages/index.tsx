bd6ab75091b0aa91f0d3d7d2e8b28f0654de68a2
Kayque Gonçalves <kayque.gsilva229@gmail.com>
2021-06-07 12:29:36 -0300

    Updates index structure

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,5 +1,11 @@
-export default function Home(): JSX.Element {
+// eslint-disable-next-line no-use-before-define
+import React from 'react'
+import { NextPage } from 'next'
+
+const IndexPage: NextPage = () => {
   return (
     <h1> Next app </h1>
   )
 }
+
+export default IndexPage
